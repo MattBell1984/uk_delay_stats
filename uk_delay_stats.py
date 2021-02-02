38b5c5b0493e625d52b6c8a810297a407f53413b
Matthew Bell <mattbell1984@gmail.com>
2021-02-02 14:38:20 +0000

    Initial commit

---
+++ uk_delay_stats.py
@@ -0,0 +1,10 @@
+#Investigate delay statistics at UK airport and compare whether the COVID-19
+#pandemic has had an effect on delays due to severe weather events
+
+import pandas as pd
+
+#read files and import rows
+
+#print total number of rows
+
+#Choose airports to compare
