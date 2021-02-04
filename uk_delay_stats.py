04ff9621db36b029bb36f21c28b1d7e86ce166da
Matthew Bell <mattbell1984@gmail.com>
2021-02-04 15:43:50 +0000

    stage commit

--- uk_delay_stats.py
+++ uk_delay_stats.py
@@ -4,7 +4,16 @@
 import pandas as pd
 
 #read files and import rows
+df_1019 = pd.read_csv('201910_Punctuality_Statistics_Full_Analysis.csv')
+df_1020 = pd.read_csv('202010_Punctuality_Statistics_Full_Analysis.csv')
+
+df = pd.concat([df_1019, df_1020], ignore_index=True )
 
 #print total number of rows
+
+print("Number of rows: " + str(len(df)))
+
+
+
 
 #Choose airports to compare
