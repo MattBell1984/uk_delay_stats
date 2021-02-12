e09b7c983ed8cbbba4198cbc2c83a0377936aae8
Matthew Bell <mattbell1984@gmail.com>
2021-02-12 16:16:45 +0000

    refactor: tidied commands to PEP8

--- uk_delay_stats.py
+++ uk_delay_stats.py
@@ -19,7 +19,9 @@ print(df.shape)
 #reshape df to only consider useful information
 #df should only have date, departure, dest, sum of early/late/canx flights
 
-df['on_time_or_early_percent'] = df['flights_more_than_15_minutes_early_percent'] + df['flights_15_minutes_early_to_1_minute_early_percent']
+df['on_time_or_early_percent'] =\
+    df['flights_more_than_15_minutes_early_percent'] +\
+    df['flights_15_minutes_early_to_1_minute_early_percent']
 
 df['delay_percent'] = df.loc[:,'flights_0_to_15_minutes_late_percent':'flights_more_than_360_minutes_late_percent'].sum(axis = 1)
 
@@ -34,7 +36,8 @@ print(df.columns)
 
 #Choose airports to compare
 
-print("There are {} unique origin countries considered".format(df['origin_destination_country'].nunique()))
+print("There are {} unique origin countries considered".format\
+    (df['origin_destination_country'].nunique()))
 
 print("There are {} unique origins".format(df['origin_destination'].nunique()))
 
