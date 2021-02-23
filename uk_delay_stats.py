3411c4a5ba7f446cf9e7b7cda61b4a559539ca2a
Matthew Bell <mattbell1984@gmail.com>
2021-02-23 12:00:14 +0000

    refactor: improved delay info by destination

--- uk_delay_stats.py
+++ uk_delay_stats.py
@@ -5,13 +5,13 @@ df_1019 = pd.read_csv('201910_Punctuality_Statistics_Full_Analysis.csv')
 df_1020 = pd.read_csv('202010_Punctuality_Statistics_Full_Analysis.csv')
 
 df = df_1019
+dfb = df_1020
 
-#Read files & combine
 
 destlist = ['HEATHROW', 'GATWICK']
 
 df1 = df[df.reporting_airport.isin(destlist)].copy()
-
+df2 = dfb[df.reporting_airport.isin(destlist)].copy()
 #Creates new df dealing with just airports named in destlist. .copy() method
 #prevents setcopywarning in delay_percent creation.
 
@@ -37,6 +37,8 @@ df1['num_flights_ontime'] = df1['num_flights_ontime'].round(0).astype(int)
 #uses percentage ontime or early to calculate raw number of flights early or
 #late
 
+
+
 print(df1.columns)
 
 
@@ -60,7 +62,7 @@ print("The unique origins are:")
 print(df1.origin_destination.unique())
 
 print("=" * 40)
-
+print("=" * 40)
 #check for NaN
 
 #print(df.isna().sum())
@@ -77,18 +79,35 @@ print("=" * 40)
 #df1 = df1.sort_values('reporting_airport')
 
 
+df2['on_time_or_early_percent'] =\
+    df2['flights_more_than_15_minutes_early_percent'] +\
+    df2['flights_15_minutes_early_to_1_minute_early_percent']
 
-print(df1.groupby(df1['origin_destination'])[['number_flights_matched',\
-    'num_flights_ontime','on_time_or_early_percent',\
-    'num_flights_delayed', 'delay_percent']].sum().head(10))
+df2['delay_percent'] = df2.loc[:,'flights_0_to_15_minutes_late_percent'\
+    :'flights_more_than_360_minutes_late_percent'].sum(axis = 1)
+
+#Creates new column summing early or late percentages
+
+df2['num_flights_delayed'] = (df2['number_flights_matched'] -\
+    df2['number_flights_cancelled']) * (df2['delay_percent'] / 100)
+
+df2['num_flights_delayed'] = df2['num_flights_delayed'].round(0).astype(int)
+
+df2['num_flights_ontime'] = (df2['number_flights_matched'] -\
+    df2['number_flights_cancelled']) * (df2['on_time_or_early_percent'] / 100)
+
+df2['num_flights_ontime'] = df2['num_flights_ontime'].round(0).astype(int)
+
+
+print(df1.groupby(['origin_destination', 'reporting_airport',\
+    'number_flights_matched']).agg({'num_flights_ontime':\
+    np.sum, 'on_time_or_early_percent': np.mean, 'num_flights_delayed':\
+    np.sum, 'delay_percent': np.mean}).head(10))
 
 #Shows raw number of delayed / early flights by destination
 
 print("=" * 40)
 
-
-
-print("=" * 40)
 
 print(df1[['reporting_period', 'reporting_airport',\
     'origin_destination_country','origin_destination',\
@@ -104,6 +123,7 @@ print(df1[['reporting_airport', 'origin_destination',\
     .groupby('origin_destination').agg(['mean','count']).sort_values\
     (by=('on_time_or_early_percent','mean'), ascending=False).head(10))
 
+print("=" * 40)
 #Sort destinations by late %
 
 print(df1[['reporting_airport', 'origin_destination',\
@@ -111,13 +131,46 @@ print(df1[['reporting_airport', 'origin_destination',\
     .groupby('origin_destination').agg(['mean','count']).sort_values\
     (by=('delay_percent','mean'), ascending=False).head(10))
 
+print("=" * 40)
+print("=" * 40)
 
+print(df2.groupby(['origin_destination', 'reporting_airport',\
+    'number_flights_matched']).agg({'num_flights_ontime':\
+    np.sum, 'on_time_or_early_percent': np.mean, 'num_flights_delayed':\
+    np.sum, 'delay_percent': np.mean}).head(10))
+
+#Shows raw number of delayed / early flights by destination
+
+print("=" * 40)
+
+
+print(df2[['reporting_period', 'reporting_airport',\
+    'origin_destination_country','origin_destination',\
+    'number_flights_matched', 'num_flights_ontime',\
+    'num_flights_delayed']].head(10))
+
+print("=" * 40)
+
+#Sort destinations by early %
+
+print(df2[['reporting_airport', 'origin_destination',\
+    'on_time_or_early_percent','num_flights_ontime']]\
+    .groupby('origin_destination').agg(['mean','count']).sort_values\
+    (by=('on_time_or_early_percent','mean'), ascending=False).head(10))
+
+print("=" * 40)
+#Sort destinations by late %
+
+print(df2[['reporting_airport', 'origin_destination',\
+    'delay_percent', 'num_flights_delayed']]\
+    .groupby('origin_destination').agg(['mean','count']).sort_values\
+    (by=('delay_percent','mean'), ascending=False).head(10))
 
 
 #TODO: Clean Data (Remove duplicates) - This is caused by different airlines
 #flying to the same destination. flights should be combined before the
 #percentages are generated
-#TODO: Organise by Date
+
 #TODO: Generate stats for delays - Mean, Median, Mode, LH / SH split
 #TODO: Pull all data together.
 #TODO: (Stretch Goal) Consider dashboard / program to allow users to explore
